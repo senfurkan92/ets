aa5c09cef08bec2b77d52c0c0cb2281b2304d0e6
senfurkan92 <senfurkan92@gmail.com>
2022-06-18 13:04:44 -0400

    bll layer added and repo pattern classess developed

---
+++ ETS_TUR/BLL/Data/Manager/Managers.cs
@@ -0,0 +1,38 @@
+﻿using BLL.Data.Service;
+using DAL.Data;
+using DOMAIN.Entities;
+
+namespace BLL.Data.Manager
+{
+    public class CategoryManager : ManagerGeneric<Category, IDalCategory>, IServiceCategory
+    {
+        public CategoryManager(IDalCategory dal) : base(dal)
+        {
+
+        }
+    }
+
+    public class ActivityManager : ManagerGeneric<Activity, IDalActivity>, IServiceActivity
+    {
+        public ActivityManager(IDalActivity dal) : base (dal)
+        {
+
+        }
+    }
+
+    public class ActivityImageManager : ManagerGeneric<ActivityImage, IDalActivityImage>, IServiceActivityImage
+    {
+        public ActivityImageManager(IDalActivityImage dal) : base(dal)
+        {
+
+        }
+    }
+
+    public class TicketManager : ManagerGeneric<Ticket, IDalTicket>, IServiceTicket
+    {
+        public TicketManager(IDalTicket dal) : base(dal)
+        {
+
+        }
+    }
+}
---
+++ ETS_TUR/BLL/Data/Service/Services.cs
@@ -0,0 +1,29 @@
+﻿using DOMAIN.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Data.Service
+{
+    public interface IServiceCategory : IService<Category>
+    { 
+    
+    }
+
+    public interface IServiceActivity : IService<Activity>
+    { 
+    
+    }
+
+    public interface IServiceActivityImage : IService<ActivityImage>
+    { 
+    
+    }
+
+    public interface IServiceTicket : IService<Ticket>
+    { 
+    
+    }
+}
